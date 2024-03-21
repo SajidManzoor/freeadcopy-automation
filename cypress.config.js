4aebd13ca58fbf3a3c858ae89ed06266d5c94b05
Sajid Manzoor <sqaprofessional1@gmail.com>
2024-03-21 15:34:53 +0500

    config changes

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,12 @@
 const { defineConfig } = require("cypress");
 const { addCucumberPreprocessorPlugin, } = require("@badeball/cypress-cucumber-preprocessor");
 const { preprocessor, } = require("@badeball/cypress-cucumber-preprocessor/browserify");
+// const allureWriter = require("@shelex/cypress-allure-plugin/writer");
 
 async function setupNodeEvents(on, config) {
   // This is required for the preprocessor to be able to generate JSON reports after each run, and more,
   await addCucumberPreprocessorPlugin(on, config);
-
+  await require('cypress-mochawesome-reporter/plugin')(on)
   on("file:preprocessor", preprocessor(config));
 
   // Make sure to return the config object as it might have been modified by the plugin.
@@ -15,10 +16,13 @@ async function setupNodeEvents(on, config) {
 module.exports = defineConfig({
   projectId: "rizcte",
   defaultCommandTimeout: 30000,
-
+  reporter: "cypress-mochawesome-reporter",
   e2e: {
     baseUrl: 'https://freeadcopy.com/',
     setupNodeEvents,
+    // setupNodeEvents(on,config){
+    //   require('cypress-mochawesome-reporter/plugin')(on)
+    // },
     experimentalRunAllSpecs: true,
     //specPattern: 'cypress/Integration/*.js'
     specPattern: 'cypress/Integration/FeatureFiles/*.feature',
@@ -27,5 +31,11 @@ module.exports = defineConfig({
     experimentalModifyObstructiveThirdPartyCode: true,
     watchForFileChanges: false,
     experimentalRunAllSpecs: true,
+    // setupNodeEvents(on, config) {
+    //   // implement node event listeners here
+    //   require("cypress-mochawesome-reporter/plugin")(on);
+    //   // allureWriter(on, config);
+    //   return config;
+    // },
   },
 });
